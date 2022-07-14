e3b89757f49e1a112b86fcde5db94d28222ae623
anti-ddos-l4 <109312219+anti-ddos-l4@users.noreply.github.com>
2022-07-15 03:00:46 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -1,5 +1,18 @@
-apt update
-echo Packages Updated.
+#!/bin/bash
+cd /
+
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt install php7.4
+sudo apt install php7.4-gd php7.4-cli php7.4-fpm php7.4-json php7.4-xml php7.4-zip php7.4-mysql php7.4-mbstring php7.4-curl
+sudo apt install php7.4-cli
+y
+sudo apt-get install -y php7.4-curl
+apt install net-tools
+sudo apt-get update
+echo "* * * * * php /autoblock.php" >> /var/spool/cron/crontabs/root
+php /autoblock.php
+clear
 sleep 1
 apt install iptables-persistent
 iptables -N SAFEZONE
